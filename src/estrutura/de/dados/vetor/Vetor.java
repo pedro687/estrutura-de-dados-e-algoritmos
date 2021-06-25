270fac2d0049ac1f27d84b31fb96bb231b7ebdec
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-06-24 21:00:25 -0300

    Aula 02

---
+++ src/estrutura/de/dados/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package estrutura.de.dados.vetor;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int quantidade) {
+        this.elementos = new String[quantidade];
+    }
+}
---
+++ src/estrutura/de/dados/vetor/teste/Aula02.java
@@ -0,0 +1,11 @@
+package estrutura.de.dados.vetor.teste;
+
+import estrutura.de.dados.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        //vetor com capacidade para elementos
+        Vetor vetor = new Vetor(5);
+
+    }
+}
